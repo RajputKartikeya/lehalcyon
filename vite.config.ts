933a107adf97498c4a6bb0afd41211ffa15aa7c3
Kartikeya Rajput <kartikeyarajput03@gmail.com>
2024-09-08 22:50:15 +0530

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,17 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    target: ["es2015", "edge88", "firefox78", "chrome87", "safari14"],
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+  server: {
+    host: true, // Needed for mobile device testing
+  },
 });
